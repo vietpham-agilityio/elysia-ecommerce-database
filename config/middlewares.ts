492103aa620616cdc35af98c19f456cfa66fd64e
Viet Pham <viet.pham@asnet.com.vn>
2025-03-05 10:29:30 +0700

    Feat: Update config CORS on middlwares.ts config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,13 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
 ];
